9858ecdfecee71b131cda47d887bbd1a498cdb9b
Supriyo Chakraborty <99415103+suprriyo@users.noreply.github.com>
2025-01-29 11:16:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-from groq import Groq
+from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
