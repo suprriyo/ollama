41dd713748b6e6bf55fe208559993d05bebdeee2
Supriyo Chakraborty <99415103+suprriyo@users.noreply.github.com>
2025-01-29 11:34:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
 # Set API key from Streamlit secrets
-os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]["api_key"]
+os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
 
 
 # Initialize Groq client
